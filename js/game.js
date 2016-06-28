1c53368ce7debce4eaf28d2c949769ab389624cb
lesa <cnw39@drexel.edu>
2016-06-28 14:48:21 -0400

    Week 2 changes
    Starting to build solo pong game.

--- js/game.js
+++ js/game.js
@@ -19,4 +19,36 @@ window.cancelRequestAnimFrame = ( function() {
 		clearTimeout;
 } )();
 
-console.log('Holla');
+//DO NOT TOUCH CODE ABOVE
+
+//console.log('Holla');
+
+// Step 01 .. cnw .. Create game canvas and track mouse position
+var gameCanvas = document.getElementById("canvas");
+// Store HTML5 canvas tag into JS variable
+
+var ctx = gameCanvas.getContext("2d");
+// Create canvas in 2d
+var W = window.innerWidth; 
+var H = window.innerHeight;
+
+var mouseObj = {};
+
+gameCanvas.width = W;
+gameCanvas.height = H;
+
+function paintCanvas() {
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, W, H); 
+}
+
+paintCanvas(); 
+
+
+function trackPosition(evt) {
+    mouseObj.x = evt.pageX;
+    mouseObj.y = evt.pageY;
+    console.log("cursor x is : " + mouseObj.x + " cursor Y is : " + mouseObj.y);
+}
+
+gameCanvas.addEventListener("mousemove", trackPosition, true);
