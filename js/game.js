b0378a810359d795e66e5104e15440363aed369a
lesa <cnw39@drexel.edu>
2016-07-12 15:01:01 -0400

    Added some functionality
    Moving forward with working sample.

--- js/game.js
+++ js/game.js
@@ -49,7 +49,7 @@ paintCanvas();
 function trackPosition(evt) {
     mouseObj.x = evt.pageX;
     mouseObj.y = evt.pageY;
-    console.log("cursor x is : " + mouseObj.x + " cursor Y is : " + mouseObj.y);
+//    console.log("cursor x is : " + mouseObj.x + " cursor Y is : " + mouseObj.y);
 }
 
 gameCanvas.addEventListener("mousemove", trackPosition, true);
@@ -105,3 +105,139 @@ function paintScore () {
     ctx.fillText("Score: " + points, 20, 20);
 }
 paintScore();
+
+// Step 06 .. cnw .. Place paddles (top and bottom) on canvas
+function paddlePosition(TB) {
+    this.w = 150;
+    this.h = 5; 
+    
+    this.x = W/2 - this.w/2;
+    
+    if (TB == "top") {
+        this.y = 0;
+    }  else {
+        this.y = H - this.h;  
+        }
+}
+
+var paddlesArray = []; // Paddles Array
+paddlesArray.push(new paddlePosition("top"));
+paddlesArray.push(new paddlePosition("bottom"));
+//console.log("top paddle y is: " + paddlesArray[0].y);
+//console.log("bottom paddle y is: " + paddlesArray[1].y);
+
+function paintPaddles() {
+    for (var lp = 0; lp < paddlesArray.length; lp++) {
+        p = paddlesArray[lp];
+        
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(p.x, p.y, p.w, p.h);
+    }
+}
+paintPaddles();
+
+// Step 07 .. cnw .. Detect when the user clicks on the screen
+gameCanvas.addEventListener("mousedown", btnClick, true);
+
+function btnClick(evt) {
+    var mx = evt.pageX; 
+    var my = evt.pageY;
+    
+    // User clicked on start button
+    if (mx >= startBtn.x && mx <= startBtn.x + startBtn.w) {
+        if (my >= startBtn.y && my <= startBtn.y + startBtn.h) {
+            //   console.log ("Start button clicked");
+            // Delete the start button
+        startBtn = {};
+            
+        // Start Game animation loop
+        animloop();
+        }
+    }
+}
+
+// Function for running the whole game animation
+var init; // Variable to initialize animation
+function animloop() {
+    init = requestAnimFrame(animloop);
+    refreshCanvasFun();
+}
+
+// Step 08 .. cnw .. Draw everything on canvas over and over again
+function refreshCanvasFun () {
+    paintCanvas();
+    paintPaddles();
+    ball.draw();
+    paintScore();
+    update();
+}
+
+function update () {
+    // move the paddles, track the mouse
+    for (var lp = 0; lp < paddlesArray.length; lp++) {
+        p = paddlesArray[lp];
+        p.x = mouseObj.x - p.w / 2;
+        
+    }
+    // move the ball
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+    // Check for ball paddle collision
+    check4collision();
+}
+
+function check4collision() {
+    var pTop = paddlesArray[0];
+    var pBot = paddlesArray[1];
+    
+    if (collides(ball, pTop)) {
+        collideAction(ball, pTop);
+    } else if (collides(ball, pBot)) {
+        collideAction(ball, pBot);
+    } else {
+        // collide with walls, or end game
+        if (ball.y + ball.r > H) {
+            // Game over
+        } else if (ball.y < 0) {
+            // Game over
+        }
+        // Ball hits the side of screen
+        if (ball.x + ball.r > W) {
+            ball.vx = -ball.vx;
+            ball.x = W - ball.r;
+        } else if (ball.x - ball.r < 0) {
+            ball.vx = -ball.vx;
+            ball.x = ball.r;
+        }
+    }
+    
+}
+
+var paddleHit; // Which paddle was hit 0=top, 1=bottom
+function collides(b, p) {
+    if (b.x + b.r >= p.x && b.x - b.r <= p.x + p.w) {
+        if (b.y >= (p.y - p.h) && p.y > 0) {
+            paddleHit = 0;
+            return true;
+        } else if (b.y <= p.h && p.y === 0) {
+            paddleHit = 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+var collisionSnd = document.getElementById("collide")
+
+function collideAction (b, p) {
+//    console.log ("sound and then bounce");
+    if (collisionSnd) {
+          collisionSnd.play();  
+    }
+    // reverse ball y velocity
+    ball.vy = - ball.vy;
+    // increase the score by 1
+    points++;
+}
+
